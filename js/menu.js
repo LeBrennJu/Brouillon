fbb9d5f47a948d7f4e4fc736dd9af91aec6cbc9c
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-07-17 23:43:06 +0200

    remise en page responsive avec prise en compte du menu

--- js/menu.js
+++ js/menu.js
@@ -1,33 +1,32 @@
 const menu = {
     init: function () {
         console.log('init menu OK');
+
+        // on initialise les classes 'hide suivant le format d'affichage
+        if (window.innerWidth < 800) {
+            document.querySelector('.menu--close.btn').classList.add('hide');
+            document.querySelector('.menu').classList.add('hide');
+
+        } else {
+            document.querySelector('.menu').classList.remove('hide');
+        }
+
         const menuBtnOpen = document.querySelector('.menu--open.btn');
         const menuBtnClose = document.querySelector('.menu--close.btn');
         menuBtnOpen.addEventListener('click', menu.handleshowMenu);
         menuBtnClose.addEventListener('click', menu.handleshowMenu);
     },
 
+
     handleshowMenu: function () {
 
         // on bascule l'affichage avec la classe 'hide' sur les bouttons du menu, la nav, le main et le footer
         document.querySelector('.menu--open.btn').classList.toggle('hide');
         document.querySelector('.menu--close.btn').classList.toggle('hide');
-
         document.querySelector('.menu').classList.toggle('hide');
+        // document.querySelector('main').classList.toggle('hide');
+        // document.querySelector('footer').classList.toggle('hide');
 
-        // pour mettre le menu sur le reste de la page, 
-        // on calcule l'espace qu'il y entre le 1er élément du menu et le bas de page
-        // let menuHaut = document.querySelector('.menu').offsetTop;
-        // let menuBas = window.innerHeight;
-        // document.querySelector('.menu--page').style.height = menuBas - menuHaut - 15 + 'px';
-
-        document.querySelector('main').classList.toggle('hide');
-        document.querySelector('footer').classList.toggle('hide');
-
-        // si le menu est ouvert, on règle les hauteurs des éléments pour les répartir uniformément sur la page
-        // document.querySelector('.menu--page').classList.toggle('menu--open--fullscreen__nav');
-        // document.querySelector('.banner').classList.toggle('menu--open--fullscreen__header');
-        // console.log(menuHaut, menuBas, menuBas - menuHaut);
     }
 
 };
