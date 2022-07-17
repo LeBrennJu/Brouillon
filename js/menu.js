f6c267ab2001c0e60f1efaad26588dabdab26656
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-07-17 11:30:50 +0200

    animation et style menu + mise en page v1 card et footer

--- js/menu.js
+++ js/menu.js
@@ -14,6 +14,9 @@ const menu = {
         document.querySelector('.menu--close.btn').classList.toggle('hide');
 
         document.querySelector('.menu--page').classList.toggle('hide');
+
+        // pour mettre le menu sur le reste de la page, 
+        // on calcule l'espace qu'il y entre le 1er élément du menu et le bas de page
         let menuHaut = document.querySelector('.menu').offsetTop;
         let menuBas = window.innerHeight;
         document.querySelector('.menu--page').style.height = menuBas - menuHaut - 15 + 'px';
@@ -24,7 +27,7 @@ const menu = {
         // si le menu est ouvert, on règle les hauteurs des éléments pour les répartir uniformément sur la page
         document.querySelector('.menu--page').classList.toggle('menu--open--fullscreen__nav');
         document.querySelector('.banner').classList.toggle('menu--open--fullscreen__header');
-        console.log(menuHaut, menuBas, menuBas - menuHaut);
+        // console.log(menuHaut, menuBas, menuBas - menuHaut);
     }
 
 };
