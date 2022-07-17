8b5bd1358135f58ac57301eaa83d36d44d263b2a
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-07-17 19:37:27 +0200

    mise en page responsive

--- js/menu.js
+++ js/menu.js
@@ -13,20 +13,20 @@ const menu = {
         document.querySelector('.menu--open.btn').classList.toggle('hide');
         document.querySelector('.menu--close.btn').classList.toggle('hide');
 
-        document.querySelector('.menu--page').classList.toggle('hide');
+        document.querySelector('.menu').classList.toggle('hide');
 
         // pour mettre le menu sur le reste de la page, 
         // on calcule l'espace qu'il y entre le 1er élément du menu et le bas de page
-        let menuHaut = document.querySelector('.menu').offsetTop;
-        let menuBas = window.innerHeight;
-        document.querySelector('.menu--page').style.height = menuBas - menuHaut - 15 + 'px';
+        // let menuHaut = document.querySelector('.menu').offsetTop;
+        // let menuBas = window.innerHeight;
+        // document.querySelector('.menu--page').style.height = menuBas - menuHaut - 15 + 'px';
 
         document.querySelector('main').classList.toggle('hide');
         document.querySelector('footer').classList.toggle('hide');
 
         // si le menu est ouvert, on règle les hauteurs des éléments pour les répartir uniformément sur la page
-        document.querySelector('.menu--page').classList.toggle('menu--open--fullscreen__nav');
-        document.querySelector('.banner').classList.toggle('menu--open--fullscreen__header');
+        // document.querySelector('.menu--page').classList.toggle('menu--open--fullscreen__nav');
+        // document.querySelector('.banner').classList.toggle('menu--open--fullscreen__header');
         // console.log(menuHaut, menuBas, menuBas - menuHaut);
     }
 
