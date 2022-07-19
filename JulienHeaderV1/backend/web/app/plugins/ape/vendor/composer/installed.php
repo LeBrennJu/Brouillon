1ac98a7ce7daf2895a3028af16eb6e0af1fc407d
Natalene35 <poupouille20@msn.com>
2022-07-19 14:14:04 +0200

    Correction fôtes titre :)

--- JulienHeaderV1/backend/web/app/plugins/ape/vendor/composer/installed.php
+++ JulienHeaderV1/backend/web/app/plugins/ape/vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '0b6d842c97ef62623ef525b017e426ebd771e882',
         'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '0b6d842c97ef62623ef525b017e426ebd771e882',
             'dev_requirement' => false,
         ),
     ),
